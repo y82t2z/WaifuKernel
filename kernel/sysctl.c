cb225f2b5b8c1d622aee923b8a30ca871e857ad3
Juansheng <jaycopter@foxmail.com>
2023-11-18 21:56:49 +0800

    sched/fair: Compile WALT stuff with CONFIG_SCHED_WALT
    Refs: Oneplus sm8350 Kernel.
    
    Disable sysctl_sched_force_lb_enable as well.

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -635,6 +635,7 @@ static struct ctl_table kern_table[] = {
 		.extra2		= &one_thousand,
 	},
 #endif
+#ifdef CONFIG_SCHED_WALT
 	{
 		.procname	= "sched_force_lb_enable",
 		.data		= &sysctl_sched_force_lb_enable,
@@ -644,6 +645,7 @@ static struct ctl_table kern_table[] = {
 		.extra1		= &zero,
 		.extra2		= &one,
 	},
+#endif
 #ifdef CONFIG_SCHED_DEBUG
 	{
 		.procname       = "sched_cstate_aware",
